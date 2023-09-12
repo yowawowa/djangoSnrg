de84ac545637f3a55a31047078e31945fed4f4d9
Vladislav Kostin <92290777+yowawowa@users.noreply.github.com>
2023-09-12 16:01:19 +0500

    media folder issue resolved

--- djangoSnrg/answers/admin.py
+++ djangoSnrg/answers/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Answer, Human, Profession
 
 
@@ -10,9 +12,19 @@ class AnswersAdmin(admin.ModelAdmin):
 
 
 class HumansAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'profession', 'height', 'address')
+    list_display = ('id', 'name', 'get_photo', 'profession', 'height', 'address')
     list_editable = ('profession', 'address')
     list_display_links = ('id', 'name',)
+    fields = ('name', 'height', 'photo', 'get_photo', 'profession', 'address')
+    readonly_fields = ('get_photo',)
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return 'no photo'
+
+        get_photo_description = 'miniature'
 
 
 class ProfessionAdmin(admin.ModelAdmin):
--- djangoSnrg/djangoSnrg/settings.py
+++ djangoSnrg/djangoSnrg/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'djangoSnrg/static')
+    os.path.join(BASE_DIR, 'djangoSnrg/static/')
 ]
 
 # Default primary key field type
@@ -129,5 +129,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = os.path.join(BASE_DIR, '/media/')
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
